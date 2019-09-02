45a5e97802323adcd436cda82aefffb4672257a9
Christian Gutierrez Salas <desarrollo11@aate.gob.pe>
2019-09-02 11:12:43 -0500

    first commit

---
+++ front_micro/src/environments/environment.prod.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  HOST_URL: 'http://192.168.99.100',
+  TOKEN_AUTH_USERNAME: 'mitomediapp',
+  TOKEN_AUTH_PASSWORD: 'mito89codex',
+  TOKEN_NAME: 'access_token',
+  REINTENTOS: 3
+};
